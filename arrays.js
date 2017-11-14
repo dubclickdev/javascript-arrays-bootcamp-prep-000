ad6ab1269890c50b68406b2365f84f6fbd1adbef
dubclickdev <battim_3c@yahoo.com>
2017-11-14 18:33:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,26 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length - 1)
+  return array
+}
